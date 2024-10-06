868a106cc497742ac9ac2c717696e7e74ae30854
erenoguzyesil <jfv.eoy@gmail.com>
2024-10-06 19:34:46 +0300

    Updated website.

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,27 @@
 const learnMoreButton = document.getElementById('learn-more-button');
-const articleElement = document.getElementById('first-article');
+const firstArticleElement = document.getElementById('first-article');
 
 learnMoreButton.addEventListener('click', () => {
-  articleElement.scrollIntoView({ behavior: 'smooth' });
+  firstArticleElement.scrollIntoView({ behavior: 'smooth' });
 });
+
+const references = document.querySelectorAll('#reference-list li');
+
+for (let i = 0; i < references.length; i++) {
+  references[i].setAttribute('id', `ref-${(i + 1)}`)
+}
+
+const articleElements = document.querySelectorAll('article');
+
+for (let i = 0; i < articleElements.length; i++) {
+  let elementContent = articleElements[i].innerHTML;
+  let matchesInsideBrackets = elementContent.match(/\[(.*)\]/g);
+
+  if (matchesInsideBrackets == null) continue;
+
+  let referenceNumbers = matchesInsideBrackets.map((x) => x[1]);
+
+  for (let j = 0; j < matchesInsideBrackets.length; j++) {
+    articleElements[i].innerHTML = articleElements[i].innerHTML.replace(matchesInsideBrackets[j], `<a href="#ref-${referenceNumbers[j]}">[${referenceNumbers[j]}]</a>`);
+  }
+}
